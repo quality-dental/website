2f826b884369c44bf1f9e40024565344eebafcce
Nordbox <ali.suliman.alx@gmail.com>
2023-05-28 09:39:09 +0200

    feat: change examin page url

--- pages/examin_arabic_399.js
+++ pages/examin_arabic_399.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import styles from "../../styles/form_page.module.scss"
+import styles from "../styles/form_page.module.scss"
 
 const formArb = () => {
   const [firstName, setFirstName] = useState("")
