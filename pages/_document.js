514bc8f482742fab18fa0f6bfe1a4b408049e3af
Nordbox <ali.suliman.alx@gmail.com>
2023-05-30 20:02:30 +0200

    feat: add fb pixels

---
+++ pages/_document.js
@@ -0,0 +1,48 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Facebook Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1940372792992821');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1940372792992821&ev=PageView&noscript=1"
+            />
+          </noscript>
+          <meta
+            name="facebook-domain-verification"
+            content="q24dlfrb2kqel3aasji5u0g71p3g91"
+          />
+          {/* End Meta Pixel Code */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
